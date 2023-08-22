793b4f40b8104afbe05908815414990b6b5999c4
Ryan <ryanwanjiedistortion@gmail.com>
2023-08-22 10:27:34 +0300

    starting with TypeScript

--- 01-intro/starter.ts
+++ 01-intro/starter.ts
@@ -2,4 +2,4 @@ let user = {name:'Wiko', age:10}
 
 let details = user.name
 
-console.log(details)
+console.log(details) 
---
+++ 01-intro/var.ts
@@ -0,0 +1,7 @@
+let greeting :string = 'Juba, Southsuz'
+
+
+console.log(greeting)
+
+ export {}
+ //removes an error temporarily 
