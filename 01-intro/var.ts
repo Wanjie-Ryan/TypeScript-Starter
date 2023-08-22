a8da4aea02357f8ccb718565e79af35f0b44ebec
Ryan <ryanwanjiedistortion@gmail.com>
2023-08-22 13:17:13 +0300

    starting with TypeScript

--- 01-intro/var.ts
+++ 01-intro/var.ts
@@ -3,5 +3,31 @@ let greeting :string = 'Juba, Southsuz'
 
 console.log(greeting)
 
+let userId:number  = 57634
+console.log(userId)
+
+function addTwo (num:number){
+
+    return num + 21
+    
+} 
+
+console.log(addTwo(21))
+
+let loginDetails = (name:string, age:number, loggedIn:boolean = false)=>{}
+
+loginDetails('Omondi', 33)
+
+
+//specifies that the specific return statement will be of type number
+
+function addTo (num:number):number{
+
+    return num + 21
+    
+} 
+
+console.log(addTo(2))
+
  export {}
  //removes an error temporarily 
